5ec95a7b580c2306de8ef8b1f1cbe9ace93c09b7
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-25 22:58:55 +0530

    add AppDbContext with DbSet of Todo

---
+++ backend/backend/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models.Entites;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace backend.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+        public DbSet<Todo> Todos { get; set; }
+    }
+}

d8aa7f58ab797f893f5bc9fca8869107a5b86ae8
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-26 00:51:12 +0530

    add dtos required for todo model

---
+++ backend/backend/Models/Dtos/AddTodoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models.DTOs
+{
+    public class AddTodoDto
+    {
+        public required string Title { get; set; }
+        public string? Description { get; set; } = string.Empty;
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime DueDate { get; set; }
+        public required string Priority { get; set; }
+        public required string Status { get; set; }
+    }
+}
---
+++ backend/backend/Models/Dtos/TodoResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models.Entites;
+
+namespace backend.Models.DTOs
+{
+    public class TodoResponseDto
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime DueDate { get; set; }
+        public required string Priority { get; set; }
+        public required string Status { get; set; }
+    }
+}

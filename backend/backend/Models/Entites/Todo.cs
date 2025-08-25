3113f1052412f64636abb02786ed3ea3822eab90
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-25 22:59:35 +0530

    add the model class for todo with attributes

---
+++ backend/backend/Models/Entites/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Models.Entites
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public string? description { get; set; }
+        public required DateOnly CreatedAt { get; set; }
+        public required DateOnly DueDate { get; set; }
+        public required string Priority { get; set; }
+        public required string Status { get; set; }
+    }
+}

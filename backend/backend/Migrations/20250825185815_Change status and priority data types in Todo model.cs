c17679c12bb7832f99b49c4915fff6922b6c913c
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-26 00:52:53 +0530

    changed migrations for updated data types of todo model

---
+++ backend/backend/Migrations/20250825185815_Change status and priority data types in Todo model.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangestatusandprioritydatatypesinTodomodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Todos",
+                newName: "Description");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Todos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Priority",
+                table: "Todos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Todos",
+                newName: "description");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Todos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Priority",
+                table: "Todos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}

2b5c1232d0c7084b9a92fff991fee92517726f2e
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-26 00:50:14 +0530

    change the data types of status and priprity of todo model

--- backend/backend/Models/Entites/Todo.cs
+++ backend/backend/Models/Entites/Todo.cs
@@ -1,13 +1,15 @@
 ﻿namespace backend.Models.Entites
 {
+    public enum Priority { Low, Medium, High }
+    public enum Status { Pending, InProgress, Completed }
     public class Todo
     {
         public Guid Id { get; set; }
         public required string Title { get; set; }
-        public string? description { get; set; }
+        public string? Description { get; set; }
         public required DateOnly CreatedAt { get; set; }
         public required DateOnly DueDate { get; set; }
-        public required string Priority { get; set; }
-        public required string Status { get; set; }
+        public required Priority Priority { get; set; }
+        public required Status Status { get; set; }
     }
 }

7416fd1b9711868c8e1810528b47d32780f4fe94
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-26 15:13:47 +0530

    inject the preload.js which contains the function to fetch data

--- frontend/main.js
+++ frontend/main.js
@@ -1,14 +1,20 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname,'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
 
-  win.loadFile('./src/index.html')
+  win.loadFile(path.join(__dirname,'src','index.html'));
 }
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});

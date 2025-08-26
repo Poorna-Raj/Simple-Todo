6f583133bc6e65cf01950cb23317b238cd6225d4
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-26 18:16:04 +0530

    add a preload function to get a todo by the id

--- frontend/preload.js
+++ frontend/preload.js
@@ -4,5 +4,9 @@ contextBridge.exposeInMainWorld('getApi',{
     fetchData: async () =>{
         const response = await fetch('http://localhost:5282/api/Todos');
         return response.json();
+    },
+    fetchById: async(id) =>{
+        const response = await fetch(`http://localhost:5282/api/Todos/${id}`);
+        return response.json();
     }
 });

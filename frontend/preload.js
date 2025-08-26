5eb5f24a3822cdc44f77445b46cf9fdd5d901cd9
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-08-27 00:08:52 +0530

    implement the function of creating a todo and return the response json object

--- frontend/preload.js
+++ frontend/preload.js
@@ -8,5 +8,15 @@ contextBridge.exposeInMainWorld('getApi',{
     fetchById: async(id) =>{
         const response = await fetch(`http://localhost:5282/api/Todos/${id}`);
         return response.json();
+    },
+    saveTodo: async(data) =>{
+        const response = await fetch('http://localhost:5282/api/Todos',{
+            method:"POST",
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: data
+        });
+        return response.json();
     }
 });
